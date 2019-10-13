21a4d1910129c3168425fdb88a543612da9ea6f7
hsinfu <hsinfu8055@gmail.com>
2019-10-13 19:22:07 +0800

    refactor run.py to ouput to excel file

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+import os
+import pandas as pd
 
 from datetime import datetime
 from dateutil.parser import parse as parse_dt
@@ -29,6 +31,17 @@ def _parse_arguments():
         help='End time. Default: now',
     )
     parser.add_argument(
+        '--website',
+        nargs='+',
+        default=['moneyudn', 'chinatimes'],
+        help='Available: ["moneyudn", "chinatimes"]',
+    )
+    parser.add_argument(
+        '--output-dir',
+        required=True,
+        help='The dir to save the excel results file',
+    )
+    parser.add_argument(
         '--global-search',
         action='store_true',
         help='If global search, then search both in the title and the description',
@@ -51,21 +64,54 @@ def _main():
     else:
         logging.basicConfig(level=logging.INFO, format=logger_format)
 
-    moneyudn_crawler = MoneyUDNCrawler(global_search=args.global_search)
-    df_moneyudn = moneyudn_crawler.run(
-        keywords=args.keywords,
-        start=args.start,
-        end=args.end)
+    df_all = pd.DataFrame()
+    if 'moneyudn' in args.website:
+        moneyudn_crawler = MoneyUDNCrawler(global_search=args.global_search)
+        df_moneyudn = moneyudn_crawler.run(
+            keywords=args.keywords,
+            start=args.start,
+            end=args.end)
+        df_all = df_all.append(df_moneyudn, ignore_index=True, sort=False)
 
-    chinatimes_crawler = ChinaTimesCrawler(global_search=args.global_search)
-    df_chinatimes = chinatimes_crawler.run(
-        keywords=args.keywords,
-        start=args.start,
-        end=args.end)
+    if 'chinatimes' in args.website:
+        chinatimes_crawler = ChinaTimesCrawler(global_search=args.global_search)
+        df_chinatimes = chinatimes_crawler.run(
+            keywords=args.keywords,
+            start=args.start,
+            end=args.end)
+        df_all = df_all.append(df_chinatimes, ignore_index=True, sort=False)
 
-    df_all = df_moneyudn.append(df_chinatimes, ignore_index=True, sort=False)
     print(df_all)
 
+    # set the output filename
+    output_fname = os.path.join(
+        args.output_dir,
+        '{}_{}.xlsx'.format(
+            args.start.strftime('%Y-%m-%dT%H:%M:%S'),
+            args.end.strftime('%Y-%m-%dT%H:%M:%S')))
+    sheet_name = 'Sheet1'
+
+    # Create a Pandas Excel writer using XlsxWriter as the engine.
+    # Also set the default datetime and date formats.
+    writer = pd.ExcelWriter(
+        output_fname,
+        engine='xlsxwriter',
+        datetime_format='yyyy-mm-d hh:mm')
+
+    # Convert the dataframe to an XlsxWriter Excel object.
+    df_all.to_excel(
+        writer,
+        columns=['keyword', 'time', 'title', 'link'],
+        sheet_name=sheet_name,
+        index=None,
+        header=True)
+
+    # Set the column widths, to make the dates clearer.
+    writer.sheets[sheet_name].set_column('B:B', 15)
+    writer.sheets[sheet_name].set_column('C:D', 60)
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.save()
 
 if __name__ == '__main__':
     _main()
